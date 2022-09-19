c61f61471dedb8a0161f1f26ffae74a0ebb7f621
Mohammad Ridwanul Alam <myworld.shoyab@gmail.com>
2022-09-20 04:09:38 +0600

    cleaning add some cred in login page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Register from "./pages/Register";
 
 function App() {
     const authChecked = useAuthCheck();
-
     return !authChecked ? (
         <div>Checking authentication....</div>
     ) : (
